f5b30b036b38dadc541aef21fc7cc15250c1c9b1
Jigar-9958 <jigarchauhan9958@gmail.com>
2026-03-12 13:09:08 +0530

    second commit

--- admin/src/utils/AxiosConfig.jsx
+++ admin/src/utils/AxiosConfig.jsx
@@ -3,7 +3,7 @@ import CheckAdminToken from "./CheckToken";
 import { LogoutWithoutNotification } from "./Logout";
 
 const api = axios.create({
-    baseURL : "https://backend-twxo.onrender.com/api"
+    baseURL : "https://backend-twxo.onrender.com"
 });
 
 api.interceptors.request.use(
