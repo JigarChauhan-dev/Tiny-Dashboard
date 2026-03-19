d0479666fe0a5e9290fadc15878145c5e75248af
Jigar-9958 <jigarchauhan9958@gmail.com>
2026-03-19 09:27:05 +0530

    second commit

--- Travel/src/utils/Logout.jsx
+++ Travel/src/utils/Logout.jsx
@@ -1,21 +1,12 @@
 import cookie from "js-cookie";
-import {useNavigate } from "react-router-dom";
 
 function Logout() {
-  // let navigate = useNavigate();
 
-  // try {
-  //   cookie.remove("token");
-  //   alert("Logout Successful");
-  //   navigate("/login");
-  // } catch (error) {
-  //   console.log(error);
-  // }
   cookie.remove("token");
    if (navigate) {
     navigate("/login");
   } else {
-    window.location.href = "/login"; // fallback
+    window.location.href = "/login"; 
   }
 }
 
