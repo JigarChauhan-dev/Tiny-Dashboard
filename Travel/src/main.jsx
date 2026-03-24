246183652fca4f903935265f66233c52c5651dda
Jigar-9958 <jigarchauhan9958@gmail.com>
2026-03-24 11:35:57 +0530

    second commit

--- Travel/src/main.jsx
+++ Travel/src/main.jsx
@@ -8,11 +8,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // createRoot(document.getElementById("root")).render(<App />);
